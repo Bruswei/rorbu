125c117de818644e5de572003131b27e1bd6c67b
Yijun Stautland <github@yps.staut.land>
2023-04-25 13:09:19 +0200

    Take appBar to myAppBar.tsx

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,5 @@
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import MyAppBar from "./components/MyAppBar";
 import { Link as RouterLink, useLocation } from "react-router-dom";
 import "./assets/styles.css";
 import norwayImage from "./../media/sea.jpg";
@@ -55,88 +56,7 @@ export default function HomePage() {
 
   return (
     <>
-      <AppBar
-        position="fixed"
-        className={
-          scrollPosition > 0 ? "blurry-appbar scrolled" : "blurry-appbar"
-        }
-        sx={{
-          backgroundColor: "transparent",
-          boxShadow: "none",
-          zIndex: 1000,
-          width: "100%",
-          animation: "slideDown 1s",
-        }}
-      >
-        <Toolbar disableGutters>
-          <Box
-            sx={{
-              margin: "0 auto",
-              maxWidth: "60%", // Set the desired maxWidth for centered content
-              width: "100%",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <Button
-              onClick={() => {
-                window.scrollTo({
-                  top: 0,
-                  behavior: "smooth",
-                });
-              }}
-              sx={{
-                textTransform: "none",
-                fontWeight: "bold",
-                color: scrollPosition > 0 ? "black" : "white",
-                transition: "color 0.3s",
-              }}
-            >
-              <Typography variant="h6" component="div">
-                Rorbu Bømlo
-              </Typography>
-            </Button>
-            <Box>
-              <Button
-                component={RouterLink}
-                to="/"
-                color="inherit"
-                sx={{
-                  textTransform: "none",
-                  color: scrollPosition > 0 ? "black" : "white",
-                  transition: "color 0.3s",
-                }}
-              >
-                Home
-              </Button>
-              <Button
-                component={RouterLink}
-                to="/pricing"
-                color="inherit"
-                sx={{
-                  textTransform: "none",
-                  color: scrollPosition > 0 ? "black" : "white",
-                  transition: "color 0.3s",
-                }}
-              >
-                Pricing
-              </Button>
-              <Button
-                component={RouterLink}
-                to="/contact"
-                color="inherit"
-                sx={{
-                  textTransform: "none",
-                  color: scrollPosition > 0 ? "black" : "white",
-                  transition: "color 0.3s",
-                }}
-              >
-                Contact
-              </Button>
-            </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
+      <MyAppBar scrollPosition={scrollPosition} />
       <Box
         className="home-background"
         sx={{
