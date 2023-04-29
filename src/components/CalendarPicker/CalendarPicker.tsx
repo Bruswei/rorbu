6ceaacb0430c5f55eaf4536d547945d4c9223801
Yijun Stautland <github@yps.staut.land>
2023-04-29 18:20:39 +0200

    Add alerts and styles

--- src/components/CalendarPicker/CalendarPicker.tsx
+++ src/components/CalendarPicker/CalendarPicker.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import { isSameDay } from "date-fns";
 import "react-calendar/dist/Calendar.css";
+import { Alert } from "@mui/material";
 import "./calendarPicker.css";
 
 function CalendarPicker({ unavailableDates }) {
   const [selectedDate, setSelectedDate] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
 
   const tileDisabled = ({ date }) => {
     return unavailableDates.some((unavailableDate) =>
@@ -23,14 +24,38 @@ function CalendarPicker({ unavailableDates }) {
       return; // Disable clicking on unavailable dates
     }
     setSelectedDate(date);
+    setShowAlert(true);
   };
 
-  const renderPopup = () => {
-    if (selectedDate) {
+  useEffect(() => {
+    if (showAlert) {
+      const timer = setTimeout(() => {
+        setShowAlert(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [showAlert]);
+
+  const renderAlert = () => {
+    if (selectedDate && showAlert) {
       return (
-        <div className="popup">
+        <Alert
+          severity="success"
+          sx={{
+            position: "fixed",
+            left: "50%",
+            transform: "translateX(-50%)",
+            bottom: "64px",
+            maxWidth: "90%",
+            borderRadius: "4px",
+            fontSize: "14px",
+            animation: "slide-in 0.3s ease-in-out",
+            animationFillMode: "forwards",
+            opacity: 0,
+          }}
+        >
           The date you picked is available for booking.
-        </div>
+        </Alert>
       );
     }
     return null;
@@ -38,16 +63,22 @@ function CalendarPicker({ unavailableDates }) {
 
   return (
     <>
-      <Calendar
-        onClickDay={handleDayClick}
-        tileDisabled={tileDisabled}
-        calendarType="US"
-        dayClassName={(date) => {
-          const isActive = selectedDate && isSameDay(date, selectedDate);
-          return isActive ? "active" : undefined;
-        }}
-      />
-      {renderPopup()}
+      <style>
+        {`
+          @keyframes slide-in {
+            from {
+              transform: translateX(-50%) translateY(50px);
+              opacity: 0;
+            }
+            to {
+              transform: translateX(-50%) translateY(0);
+              opacity: 1;
+            }
+          }
+        `}
+      </style>
+      <Calendar onClickDay={handleDayClick} tileDisabled={tileDisabled} />
+      {renderAlert()}
     </>
   );
 }
