8347c78e22f91b15111d2f7364992d792778fb62
Yijun Stautland <github@yps.staut.land>
2023-04-30 19:10:18 +0200

    Add backdrop to calendar

--- src/components/CalendarPicker/CalendarPicker.tsx
+++ src/components/CalendarPicker/CalendarPicker.tsx
@@ -1,30 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Calendar } from "react-multi-date-picker";
 import { addMonths, isSameDay } from "date-fns";
+import { Backdrop, CircularProgress } from "@mui/material";
 import "./CalendarPicker.css";
 
 function CalendarPicker({ unavailableDates }) {
   const today = new Date();
   const defaultStartDate = today.toISOString();
   const defaultEndDate = addMonths(today, 1).toISOString();
-
-  const isDateUnavailable = (date) =>
-    unavailableDates.some((unavailableDate) =>
-      isSameDay(date, unavailableDate)
-    );
+  const [loading, setLoading] = useState(true);
 
   const mapDays = ({ date }) => {
     let props = {};
     unavailableDates.some((unavailableDate) => {
       const d = new Date(date);
       if (
-        d.toLocaleString(`default`, { month: "long" }) ===
-          unavailableDate.toLocaleString(`default`, { month: "long" }) &&
+        d.toLocaleString("default", { month: "long" }) ===
+          unavailableDate.toLocaleString("default", { month: "long" }) &&
         d.getDate() === unavailableDate.getDate()
       ) {
         props.style = {
           ...props.style,
-          color: "#0074d9",
+          color: "#ffffff",
           backgroundColor: "#f06b24",
         };
       }
@@ -33,8 +30,16 @@ function CalendarPicker({ unavailableDates }) {
     return props;
   };
 
+  // Simulating data loading delay
+  setTimeout(() => {
+    setLoading(false);
+  }, 2000);
+
   return (
     <div className="calendar-container">
+      <Backdrop open={loading} sx={{ zIndex: 999 }}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <Calendar
         multiple
         fullYear
