8050839536837d7672cc33e7c29b79c7e2411471
Yijun Stautland <github@yps.staut.land>
2023-05-18 22:32:29 +0200

    Add Mantine calendar instead

--- src/components/CalendarPicker/CalendarPicker.tsx
+++ src/components/CalendarPicker/CalendarPicker.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { DateRangePicker } from "react-date-range";
+import { Group, Indicator } from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
+import { Calendar } from "@mantine/dates";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import "./CalendarPicker.css";
@@ -11,22 +14,43 @@ function CalendarPicker({ onRangeSelected }) {
     key: "selection",
   });
 
-  const handleRangeSelect = (ranges) => {
-    setSelectedRange(ranges.selection);
-    onRangeSelected(ranges.selection);
+  const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
+
+  const changeDatePickerRange = (range: [Date | null, Date | null]) => {
+    setValue(range);
   };
 
   return (
     <div className="calendar-container">
       <div className="date-range-container">
-        <DateRangePicker
-          ranges={[selectedRange]}
-          onChange={handleRangeSelect}
-          months={2}
-          direction="horizontal"
-          showSelectionPreview={true}
-          showMonthAndYearPickers={false}
-        />
+        <Group position="center">
+          <DatePicker
+            type="range"
+            numberOfColumns={2}
+            value={value}
+            onChange={changeDatePickerRange}
+            renderDay={(date) => {
+              const day = date.getDate();
+              return (
+                <Indicator
+                  size={6}
+                  color="red"
+                  offset={-2}
+                  disabled={day !== 16}
+                >
+                  <div>{day}</div>
+                </Indicator>
+              );
+            }}
+            excludeDate={(date) => {
+              return (
+                date.getDate() === 10 ||
+                date.getDate() === 11 ||
+                date.getDate() === 12
+              );
+            }}
+          />
+        </Group>
       </div>
     </div>
   );
