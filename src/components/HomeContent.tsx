0c0de42e4530d8c39664f3c694489f28bafbe64d
Yijun <github@yps.staut.land>
2023-08-05 07:38:58 +0200

    Add initial scroll down icon

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import norwayImage from "../../media/sea.jpg";
 import { useTranslation } from "react-i18next";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 interface HomeContentProps {
   titleRef: React.RefObject<HTMLDivElement>;
@@ -59,6 +60,15 @@ export default function HomeContent(props: HomeContentProps) {
             {welcomeWords[2]}
           </span>
         </Typography>
+        <Box className="slideUpIcon" sx={{ textAlign: "center", mt: 4 }}>
+          <ExpandMoreIcon
+            sx={{
+              fontSize: "3rem",
+              animation: "bounce 1s infinite",
+              color: "primary",
+            }}
+          />
+        </Box>
       </Box>
       <Box
         sx={{
