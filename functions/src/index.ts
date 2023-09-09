e72c9d3ee770a733858b2b02652ffe58a31a929f
Yijun <github@yps.staut.land>
2023-09-09 14:28:59 +0200

    Add nodemailer to firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,81 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import {z} from "zod";
+import * as nodemailer from "nodemailer";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const onNewReservation = functions
+    .region("europe-west1")
+    .firestore.document("reservations/{reservationId}")
+    .onWrite(async (change, context) => {
+    // To get the data of the created document, use `change.after.data()`
+      const newData = change.after.data();
+
+      if (!newData) {
+        return {success: false, message: "No data found"};
+      }
+
+      const reservationSchema = z.object({
+        id: z.string(),
+        name: z.string(),
+        email: z.string().email(),
+        phone: z.string().min(8).max(30),
+        start: z.string(),
+        end: z.string(),
+        guests: z.number(),
+        message: z.string(),
+      });
+      const reservation = reservationSchema.safeParse(newData);
+      if (!reservation.success) {
+        return {success: false, message: "Invalid reservation data."};
+      }
+
+      try {
+        await admin.firestore()
+            .collection("reservations")
+            .add(reservation.data);
+
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: functions.config().gmail.email.user,
+            pass: functions.config().gmail.email.password,
+          },
+        });
+
+        const mailOptions = {
+          from: functions.config().gmail.email.user,
+          to: functions.config().gmail.email.test,
+          subject: "New reservation from " + reservation.data.name,
+          text:
+          // "Name: " +
+          // reservation.data.name +
+          // "\n" +
+          // "Email: " +
+          // reservation.data.email +
+          // "\n" +
+          // "Phone: " +
+          // reservation.data.phone +
+          // "\n" +
+          // "Start: " +
+          // reservation.data.start +
+          // "\n" +
+          // "End: " +
+          // reservation.data.end +
+          // "\n" +
+          // "Guests: " +
+          // reservation.data.guests +
+          // "\n" +
+          // "Message: " +
+          // reservation.data.message,
+          "test email works maybe?",
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        return {success: true};
+      } catch (error) {
+        return {success: false, message: "Error handling reservation"};
+      }
+    });
