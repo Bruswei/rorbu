4f81b800c1abec1deebac613f668e4ecda7cc7b8
Yijun2 <yijun2@yijun2s-mbp.lan>
2024-06-05 21:08:58 +0200

    Add env enviroment and exmaple env

--- src/backend/firebaseConfig.ts
+++ src/backend/firebaseConfig.ts
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
+// src/backend/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8S-yeKOa35_t--eF3K7vqymvW8q75FKo",
-  authDomain: "filipson-rorbu.firebaseapp.com",
-  projectId: "filipson-rorbu",
-  storageBucket: "filipson-rorbu.appspot.com",
-  messagingSenderId: "1041120419825",
-  appId: "1:1041120419825:web:ad5821c173e14b6f612e50",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
